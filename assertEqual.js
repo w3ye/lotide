83fe0625d7671a516a67566327b93013f99dc94e
William Ye <w3ye@pm.me>
2021-07-19 16:41:00 -0400

    assertEqual.js added

---
+++ assertEqual.js
@@ -0,0 +1,4 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  else console.log(`🛑🛑🛑 Assertion passed: ${actual} === ${expected}`);
+};
